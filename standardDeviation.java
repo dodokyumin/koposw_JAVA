5a66fe30c801b783841d53a658f48211a108ac67
HONGKYUMIN (ANTHONY) <95400620+dodokyumin@users.noreply.github.com>
2022-04-01 19:20:06 +0900

    Update standardDeviation.java

--- standardDeviation.java
+++ standardDeviation.java
@@ -27,22 +27,19 @@ public class standardDeviation {
 			printList(a - 1);
 
 		}
-		
+
 	}
 
 	public static void calVariance(int numA) {
 		double SumOfDoubleDeviation = 0;
-		
 		mean = (double) sum / (double) numA;
-		
-		for ( int i=0; i<numA; i++) {
-			SumOfDoubleDeviation += Math.pow(i - mean,2);
+
+		for (int i = 1; i <= numA; i++) {
+			SumOfDoubleDeviation += Math.pow(i - mean, 2);
 		}
-		
+
 		variance = SumOfDoubleDeviation / numA;
-		
 		standardDeviation = Math.sqrt(variance);
-	
 		System.out.println(standardDeviation);
 
 	}
